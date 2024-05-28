13e2a9ce66f59ba0e643de22649d6225f88dc479
iiingkeep <yeonjin1939@gmail.com>
2024-05-29 01:43:20 +0900

    video를 업로드한 당사자만 video 수정/삭제 할 수 있도록 설정

--- src/models/User.js
+++ src/models/User.js
@@ -12,9 +12,13 @@ const userSchema = new mongoose.Schema({
   videos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Video'}]
 });
 
+// users collection에 데이터를 저장하기 전 password 해시 후 저장
 // this는 Users가 create될 때 유저가 입력한 value
+// 계정 생성이나 password 수정할 때를 제외한 user.save() 사용 시 이미 hash된 password가 또 hash되지 않도록 isModified()함수로 해당 항목에 변경이 일어날 때만 동작하도록 설정
 userSchema.pre('save', async function() {
-  this.password = await bcrypt.hash(this.password, 5);
+  if(this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 })
 
 const User = mongoose.model('User', userSchema);
