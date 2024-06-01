395d933d3ce2442afe58820295fc66e261e48de8
iiingkeep <yeonjin1939@gmail.com>
2024-06-01 22:56:41 +0900

    video play에 관한 설정 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,27 @@
-console.log('video player');
+const video = document.querySelector('video');
+const playBtn = document.getElementById('play');
+const muteBtn = document.getElementById('mute');
+const time = document.getElementById('time');
+const volume = document.getElementById('volume');
+
+// video가 멈춰있을 때 클릭하면 play, 재생중일 때 클릭하면 pause
+const handlePlayClick = (e) => {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePause = () => {playBtn.innerText = 'Play';}
+const handlePlay = () => {playBtn.innerText = 'Pause';}
+
+const handleMute = (e) => {
+};
+
+
+
+
+playBtn.addEventListener('click', handlePlayClick);
+muteBtn.addEventListener('click', handleMute);
+video.addEventListener('pause', handlePause);
+video.addEventListener('play', handlePlay);
