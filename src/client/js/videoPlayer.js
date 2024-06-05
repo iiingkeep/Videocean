0bc28e24087f5d59aa8af788b38c506bba709f13
iiingkeep <yeonjin1939@gmail.com>
2024-06-05 18:18:10 +0900

    Fullscreen 기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,6 +5,8 @@ const volumRange = document.getElementById('volume');
 const currenTime = document.getElementById('currentTime');
 const totalTime = document.getElementById('totalTime');
 const timeline = document.getElementById('timeline');
+const fullScreenBtn = document.getElementById('fullScreen');
+const videoContainer = document.getElementById('videoContainer')
 
 
 let volumeValue = 0.5;
@@ -68,9 +70,21 @@ const handleTimelineChange = (e) => {
   video.currentTime = value;
 }
 
+const handleFullscreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = 'Enter Full Screen'
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = 'Exit Full Screen'
+  }
+}
+
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMuteClick);
 volumRange.addEventListener('input', handleVolumeChange);
 video.addEventListener('loadedmetadata', handleLoadedMetadata);
 video.addEventListener('timeupdate', handleTimeUpdate);
 timeline.addEventListener('input', handleTimelineChange);
+fullScreenBtn.addEventListener('click', handleFullscreen);
