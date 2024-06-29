bc17a78736574b2313504b6a838b1eb50cae8219
iiingkeep <yeonjin1939@gmail.com>
2024-06-30 01:47:43 +0900

    실시간 녹화 영상 다운로드시 영상의 1초부분을 썸네일로 하는 스크린샷을 함께 다운로드 하도록 기능 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -23,17 +23,28 @@ const handleDownload = async() => {
   // input으로 recording.webm파일을 받아 output으로 output.mp4로 변환. (recording.webm => output.mp4)
   // -r, 60: 영상을 초당 60프레임으로 인코딩하여 더 빠르게 영상 인코딩
   await ffmpeg.exec(['-i', 'recording.webm', '-r', '60', 'output.mp4']);
+  // 영상의 첫 번째 프레임(=00:00:01초)의 스크린샷을 1번 찍음
+  await ffmpeg.exec(['-i', 'recording.webm', '-ss', '00:00:01', '-frames:v', '1', 'thumbnail.jpg']);
+  
 
   const mp4File = await ffmpeg.readFile('output.mp4');
+  const thumbFile = await ffmpeg.readFile('thumbnail.jpg')
 
   const mp4Blob = new Blob([mp4File.buffer], {type: 'video/mp4'});
+  const thumbBlob = new Blob([thumbFile.buffer], {type: 'image/jpg'});
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   const a = document.createElement('a');
   a.href = mp4Url;
   a.download = 'MyRecording.mp4';
   document.body.appendChild(a);
   a.click();
+  const thumbA = document.createElement('a');
+  thumbA.href = thumbUrl;
+  thumbA.download = 'MyThumbnail.jpg';
+  document.body.appendChild(thumbA);
+  thumbA.click();
 }
 
 const handleStop = () => {
