00ba0b1f95c9719ab7d1bcac7f66fc2b5cfd23fd
iiingkeep <yeonjin1939@gmail.com>
2024-06-30 15:11:32 +0900

    영상 녹화 후 다운로드시 버튼 비활성화, 다운로드를 마치면 녹화 재시작 기능 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,14 +1,32 @@
 import {  FFmpeg } from '@ffmpeg/ffmpeg';
 import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
-const startBtn = document.getElementById('startBtn');
+const actionBtn = document.getElementById('actionBtn');
 const video = document.getElementById("preview");
 
 let stream;
 let recorder;
 let videoFile;
 
+const files = {
+  input: 'recording.webm',
+  output: 'output.mp4',
+  thumb: 'thumbnail.jpg',
+};
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement('a');
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+}
+
 const handleDownload = async() => {
+  actionBtn.removeEventListener('click', handleDownload);
+  actionBtn.innerText = 'Transcoding...'
+  actionBtn.disabled = true;
+
   const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
   const ffmpeg = new FFmpeg();
   ffmpeg.on("log", ({ message }) => {
@@ -19,55 +37,52 @@ const handleDownload = async() => {
     wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
   });
 
-  await ffmpeg.writeFile('recording.webm', await fetchFile(videoFile)); // 가상 컴퓨터에 파일 생성
+  await ffmpeg.writeFile(files.input, await fetchFile(videoFile)); // 가상 컴퓨터에 파일 생성
   // input으로 recording.webm파일을 받아 output으로 output.mp4로 변환. (recording.webm => output.mp4)
   // -r, 60: 영상을 초당 60프레임으로 인코딩하여 더 빠르게 영상 인코딩
-  await ffmpeg.exec(['-i', 'recording.webm', '-r', '60', 'output.mp4']);
+  await ffmpeg.exec(['-i', files.input, '-r', '60', files.output]);
   // 영상의 첫 번째 프레임(=00:00:01초)의 스크린샷을 1번 찍음
-  await ffmpeg.exec(['-i', 'recording.webm', '-ss', '00:00:01', '-frames:v', '1', 'thumbnail.jpg']);
+  await ffmpeg.exec(['-i', files.input, '-ss', '00:00:01', '-frames:v', '1', files.thumb]);
   
 
-  const mp4File = await ffmpeg.readFile('output.mp4');
-  const thumbFile = await ffmpeg.readFile('thumbnail.jpg')
+  const mp4File = await ffmpeg.readFile(files.output);
+  const thumbFile = await ffmpeg.readFile(files.thumb)
 
   const mp4Blob = new Blob([mp4File.buffer], {type: 'video/mp4'});
   const thumbBlob = new Blob([thumbFile.buffer], {type: 'image/jpg'});
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement('a');
-  a.href = mp4Url;
-  a.download = 'MyRecording.mp4';
-  document.body.appendChild(a);
-  a.click();
-  const thumbA = document.createElement('a');
-  thumbA.href = thumbUrl;
-  thumbA.download = 'MyThumbnail.jpg';
-  document.body.appendChild(thumbA);
-  thumbA.click();
+  downloadFile(mp4Url, 'MyRecording.mp4');
+  downloadFile(thumbUrl, 'MyThumbnail.jpg');
 
-  await ffmpeg.deleteFile('recording.webm');
-  await ffmpeg.deleteFile('output.mp4');
-  await ffmpeg.deleteFile('thumbnail.jpg');
+  await ffmpeg.deleteFile(files.input);
+  await ffmpeg.deleteFile(files.output);
+  await ffmpeg.deleteFile(files.thumb);
 
   URL.revokeObjectURL(videoFile);
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
+
+  actionBtn.disabled = false;
+  init();
+  actionBtn.innerText = 'Record Again';
+  actionBtn.addEventListener('click', handleStart);
 }
 
 const handleStop = () => {
-  startBtn.innerText = 'Download Recording'
-  startBtn.removeEventListener('click', handleStop);
-  startBtn.addEventListener('click', handleDownload);
+  actionBtn.innerText = 'Download Recording'
+  actionBtn.removeEventListener('click', handleStop);
+  actionBtn.addEventListener('click', handleDownload);
   recorder.stop();
 };
 
 // createObjectURL: 브라우저 메모리에서만 가능한 URL 생성
 // 이벤트 발생 시 얻는 video data URL을 videoFile에 할당
 const handleStart = () => {
-  startBtn.innerText = "Stop Recording";
-  startBtn.removeEventListener("click", handleStart);
-  startBtn.addEventListener("click", handleStop);
+  actionBtn.innerText = "Stop Recording";
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop);
   recorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
@@ -93,4 +108,4 @@ const init = async() => {
 
 init();
 
-startBtn.addEventListener('click', handleStart);
+actionBtn.addEventListener('click', handleStart);
